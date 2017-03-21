21ae7da5da62fa22d7a2a5b59fc4874616796d8f
klest <gonleef@tuta.io>
2017-03-21 13:22:18 +0500

    Update Main Class

--- MTDGame/Game1.cs
+++ MTDGame/Game1.cs
@@ -2,9 +2,10 @@
 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Media;
 using Microsoft.Xna.Framework.Input;
 
-namespace MTDGame
+namespace MG
 {
 	/// <summary>
 	/// This is the main type for your game.
@@ -13,10 +14,22 @@ namespace MTDGame
 	{
 		GraphicsDeviceManager graphics;
 		SpriteBatch spriteBatch;
+		Player mainPlayer = new Player();
+		Vector2 backgroundPosition;
+		Building building;
+
+		public static Game1 Instance { get; private set; }
+		public static Viewport Viewport { get { return Instance.GraphicsDevice.Viewport; } }
+		public static Vector2 ScreenSize { get { return new Vector2(Viewport.Width, Viewport.Height); } }
+
+		IO io;
 
 		public Game1()
 		{
+			Instance = this;
 			graphics = new GraphicsDeviceManager(this);
+			graphics.PreferredBackBufferWidth = 800;
+			graphics.PreferredBackBufferHeight = 600;
 			Content.RootDirectory = "Content";
 		}
 
@@ -29,8 +42,8 @@ namespace MTDGame
 		protected override void Initialize()
 		{
 			// TODO: Add your initialization logic here
-
 			base.Initialize();
+			IsMouseVisible = true;
 		}
 
 		/// <summary>
@@ -41,6 +54,15 @@ namespace MTDGame
 		{
 			// Create a new SpriteBatch, which can be used to draw textures.
 			spriteBatch = new SpriteBatch(GraphicsDevice);
+			TextureLoader.LoadContent(Content);
+
+			mainPlayer.Initialize(Game1.ScreenSize / 2);
+			building = new Building(new Vector2(500, 500));
+			CollisionComtroller.Add(mainPlayer);
+			CollisionComtroller.Add(building);
+
+			backgroundPosition = new Vector2(0, 0);
+			io = new IO(mainPlayer);
 
 			//TODO: use this.Content to load your game content here 
 		}
@@ -54,14 +76,18 @@ namespace MTDGame
 		{
 			// For Mobile devices, this logic will close the Game when the Back button is pressed
 			// Exit() is obsolete on iOS
-#if !__IOS__ && !__TVOS__
-			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-				Exit();
-#endif
+			#if !__IOS__ && !__TVOS__
+						if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+							Exit();
+			#endif
 
 			// TODO: Add your update logic here
-
+			io.Update(Keyboard.GetState(), Mouse.GetState(), gameTime);
+			CollisionComtroller.Update();
+			mainPlayer.Update(gameTime);
 			base.Update(gameTime);
+
+
 		}
 
 		/// <summary>
@@ -73,7 +99,15 @@ namespace MTDGame
 			graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
 
 			//TODO: Add your drawing code here
-
+			spriteBatch.Begin(SpriteSortMode.Deferred,
+				BlendState.AlphaBlend,
+				null, null, null, null,
+			    mainPlayer.GetCameraMatrix()
+			);
+			spriteBatch.Draw(TextureLoader.Background, backgroundPosition, Color.White);
+			mainPlayer.Draw(spriteBatch);
+			building.Draw(spriteBatch);
+			spriteBatch.End();
 			base.Draw(gameTime);
 		}
 	}
