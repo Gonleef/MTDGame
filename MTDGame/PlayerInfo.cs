5165b4415223e9c55a3bfb9c9c69b7846897b796
Avoocadoo <Laughing_boy@mail.ru>
2017-05-24 09:51:46 +0500

    Вывод здоровья на экран над персонажем.

---
+++ MTDGame/PlayerInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+namespace MG
+{
+    public class PlayerInfo
+    {
+        SpriteFont font;
+        public PlayerInfo(ContentManager content)
+        {
+            font = content.Load<SpriteFont>("spritefont");
+        }
+
+        public void DrawHealth(SpriteBatch spriteBatch)
+        {
+            if (Game1.mainPlayer.GetComponent<Health>()._health != 0)
+            {
+                spriteBatch.DrawString(font, "Health: " + Game1.mainPlayer.GetComponent<Health>()._health.ToString(),
+                    new Vector2(Game1.mainPlayer.GetComponent<Position>().position.X - Game1.mainPlayer.GetComponent<Visible>().Texture.Width,
+                    Game1.mainPlayer.GetComponent<Position>().position.Y - Game1.mainPlayer.GetComponent<Visible>().Texture.Height * 3 / 2), Color.White);
+            }
+            else
+            {
+                spriteBatch.DrawString(font, "Dead",
+                    new Vector2(Game1.mainPlayer.GetComponent<Position>().position.X - Game1.mainPlayer.GetComponent<Visible>().Texture.Width,
+                    Game1.mainPlayer.GetComponent<Position>().position.Y - Game1.mainPlayer.GetComponent<Visible>().Texture.Height * 3 / 2), Color.White);
+            }
+        }
+    }
+}
